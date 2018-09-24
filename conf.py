d728f7f8031cedb9ced33da571b0d9b7fd6bf118
Marek Suchánek <suchama4@fit.cvut.cz>
2018-09-24 18:09:19 +0200

    Building docs with Travis

--- conf.py
+++ conf.py
@@ -43,6 +43,7 @@ extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.mathjax',
     'sphinx.ext.ifconfig',
+    'sphinx.ext.doctest',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
