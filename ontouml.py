fddf1b1c7fd3c9a1f16d453dd0736eff5d43eb00
Marek Suchánek <marek.suchanek@fit.cvut.cz>
2018-10-23 19:52:51 +0200

    Links from generated info

--- ontouml.py
+++ ontouml.py
@@ -4,6 +4,7 @@ import sphinx
 import os
 import yaml
 from sphinx.util import logging
+from sphinx.util.nodes import nested_parse_with_titles
 
 
 def add_df_item(root, term, *contents):
@@ -16,51 +17,6 @@ def add_df_item(root, term, *contents):
 
 def yes_no(b):
     return 'yes' if b else 'no'
-
-
-# TODO: make cross linking by default
-def make_class_stereotype(data):
-    x = nodes.definition_list()
-    add_df_item(x, 'Category', nodes.paragraph(text=data['parent']))
-    add_df_item(x, 'Provides identity', nodes.paragraph(text=yes_no(data['provides_identity'])))
-    add_df_item(x, 'Identity principle', nodes.paragraph(text=data['identity_principle']))
-    add_df_item(x, 'Rigidity', nodes.paragraph(text=data['rigidity']))
-    add_df_item(x, 'Dependency', nodes.paragraph(text=data['dependency']))
-    add_df_item(x, 'Allowed supertypes', nodes.paragraph(text=', '.join(data['supertypes'])))
-    add_df_item(x, 'Allowed subtypes', nodes.paragraph(text=', '.join(data['subtypes'])))
-    add_df_item(x, 'Forbidden associations', nodes.paragraph(text=', '.join(data['forbidden_associations'])))
-    add_df_item(x, 'Abstract', nodes.paragraph(text=data['abstract']))
-    return x
-
-
-def make_relationship_end(data):
-    x = nodes.definition_list()
-    if data['allowed'] != '*':
-        add_df_item(x, 'Allowed', nodes.paragraph(text=', '.join(data['allowed'])))
-    low = data['min']
-    high = data['max']
-    add_df_item(x, 'Multiplicity', nodes.paragraph(text=f'{low} - {high}'))
-    if data['readOnly'] != 'undefined':
-        add_df_item(x, '{readOnly}', nodes.paragraph(text=data['readOnly']))
-    return x
-
-
-def make_binary_properties(data):
-    x = nodes.definition_list()
-    for key, value in data.items():
-        if value == 'undefined':
-            continue
-        add_df_item(x, key.title(), nodes.paragraph(text=yes_no(value)))
-    return x
-
-
-def make_relationship_stereotype(data):
-    x = nodes.definition_list()
-    add_df_item(x, 'Directed', nodes.paragraph(text=yes_no(data['directed'])))
-    add_df_item(x, 'Source end', make_relationship_end(data['source_end']))
-    add_df_item(x, 'Target end', make_relationship_end(data['target_end']))
-    add_df_item(x, 'Binary properties', make_binary_properties(data['binary_properties']))
-    return x
 
 
 class StereotypeDirective(Directive):
@@ -88,14 +44,67 @@ class StereotypeDirective(Directive):
 
         result = []
         if 'class_stereotype' in data:
-            result.append(make_class_stereotype(data['class_stereotype']))
+            result.append(self.make_class_stereotype(data['class_stereotype']))
         elif 'relationship_stereotype' in data:
-            result.append(make_relationship_stereotype(data['relationship_stereotype']))
+            result.append(self.make_relationship_stereotype(data['relationship_stereotype']))
         elif 'aggregation_stereotype' in data:
-            result.append(make_relationship_stereotype(data['aggregation_stereotype']))
+            result.append(self.make_relationship_stereotype(data['aggregation_stereotype']))
         else:
             logger.warning(f'Unknown entity in {file}')
         return result
+
+    def ref(self, label, title=None):
+        title = title or label
+        label = label[0].lower() + label[1:]
+        return f':ref:`{title} <{label}>`'
+
+    def make_ref(self, label, title=None):
+        return self.parse(self.ref(label, title))
+
+    def parse(self, to_parse):
+        para = nodes.paragraph()
+        self.state.nested_parse(nodes.paragraph(text=to_parse), 0, para)
+        return para
+
+    def make_class_stereotype(self, data):
+        x = nodes.definition_list()
+        add_df_item(x, 'Category', nodes.paragraph(text=data['parent']))
+        add_df_item(x, 'Provides identity', self.make_ref('identity', yes_no(data['provides_identity'])))
+        add_df_item(x, 'Identity principle', self.make_ref('identity', data['identity_principle']))
+        add_df_item(x, 'Rigidity', self.make_ref('rigidity', data['rigidity']))
+        add_df_item(x, 'Dependency', self.make_ref('dependency', data['dependency']))
+        add_df_item(x, 'Allowed supertypes', self.parse(', '.join(self.ref(x) for x in data['supertypes'])))
+        add_df_item(x, 'Allowed subtypes', self.parse(', '.join(self.ref(x) for x in data['subtypes'])))
+        add_df_item(x, 'Forbidden associations', self.parse(', '.join(self.ref(x) for x in data['forbidden_associations'])))
+        add_df_item(x, 'Abstract', nodes.paragraph(text=data['abstract']))
+        return x
+
+    def make_relationship_end(self, data):
+        x = nodes.definition_list()
+        if data['allowed'] != '*':
+            add_df_item(x, 'Allowed', self.parse(', '.join(self.ref(x) for x in data['allowed'])))
+        low = data['min']
+        high = data['max']
+        add_df_item(x, 'Multiplicity', nodes.paragraph(text=f'{low} - {high}'))
+        if data['readOnly'] != 'undefined':
+            add_df_item(x, '{readOnly}', nodes.paragraph(text=data['readOnly']))
+        return x
+
+    def make_binary_properties(self, data):
+        x = nodes.definition_list()
+        for key, value in data.items():
+            if value == 'undefined':
+                continue
+            add_df_item(x, key.title(), nodes.paragraph(text=yes_no(value)))
+        return x
+
+    def make_relationship_stereotype(self, data):
+        x = nodes.definition_list()
+        add_df_item(x, 'Directed', nodes.paragraph(text=yes_no(data['directed'])))
+        add_df_item(x, 'Source end', self.make_relationship_end(data['source_end']))
+        add_df_item(x, 'Target end', self.make_relationship_end(data['target_end']))
+        add_df_item(x, 'Binary properties', self.make_binary_properties(data['binary_properties']))
+        return x
 
 
 def setup(app):
